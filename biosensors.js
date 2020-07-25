16291bc9b58213190f2d636c2200db4e1e60912b
Hussam <halshehri@deakin.edu.au>
2020-07-25 22:30:28 +1000

    update to new style

--- biosensors.js
+++ biosensors.js
@@ -1,38 +1,30 @@
 
-  class Biosensor
-  {
-     constructor(name)
-     {
+  class Biosensor {
+     constructor(name) {
          // initialise with onwer and track heart rate
             this.owner=name;
             this.heartRate=0;
-            this.live()
-        
+            this.live();
      }
 
-     generatRandomBetween(min ,max)
-     {
+     generatRandomBetween(min ,max){
         return parseInt(Math.random() * (max -min ) + min );
-        
      }
 
-
-    generateHearBeat()
-    {
-      let heartBeat=this.generatRandomBetween(70,80);
+    generateHearBeat() {
+      let heartBeat=this.generatRandomBetween(60,70);
       this.heartRate=heartBeat;
-      console.log('[owner]:'+ this.owner + '---- [HR] '+ this.heartRate)
+      console.log( this.owner + ' heartbeat ='+ this.heartRate);
     }
 
-    live()
-    {
+    live(){
         setInterval(()=>{this.generateHearBeat()},1000 );
-
     }
+
   }
 
-  let james= new Biosensor("james")
-  let sara= new Biosensor("sara")
-  
+  let james= new Biosensor("Andrea");
+  let sara= new Biosensor("James ");
+
  
   
